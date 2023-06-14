a69c9120cdea5ad9b104a93362b7faffce0d0218
Im-Arjun-Gupta <guptaarjun519@gmail.com>
2023-06-14 17:58:35 +0530

    Uploading the LoginApp

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>
+      <link to = "/About">About</link>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>
+      <link to = "/Contact">Contact</link>
+    </div>
+  )
+}
+
+export default Contact
